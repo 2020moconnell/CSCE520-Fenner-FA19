be59587afca553019329fa334c187bc02603e1e9
Maggie O'Connell <32147775+2020moconnell@users.noreply.github.com>
2020-01-15 16:33:22 -0500

    Delete hw1.sql

--- p1/hw1.sql
+++
@@ -1,7 +0,0 @@
-select name, owner from pet where sex = 'f' and birth > 1990-1-1 order by name;
-
-UPDATE pet SET name = 'Maggieo' WHERE name = 'Whistler';
-
-UPDATE pet SET death = current_date WHERE name = 'Buffy';
-
-SELECT * FROM pet;
